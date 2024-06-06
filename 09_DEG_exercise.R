7f447483e2cba7b5ea44c26857cd1fe5b4bd170f
lcolladotor <lcolladotor@gmail.com>
2024-06-06 15:01:39 -0400

    Fixed the tangle'd .R files + removed a chapter we won't show in 2024
    Co-authored-by: Melissa Mayen-Quiroz <mmayen@lcg.unam.mx>
    Co-authored-by: Daianna Gonzalez-Padilla <glezdaianna@gmail.com>

--- 09_DEG_exercise.R
+++
@@ -1,9 +0,0 @@
-## ----eval = FALSE------------------------------------------------------------------------------------------------------------
-## recount3::create_rse_manual(
-##     project = "BRAIN",
-##     project_home = "data_sources/gtex",
-##     organism = "human",
-##     annotation = "gencode_v26",
-##     type = "gene"
-## )
-
