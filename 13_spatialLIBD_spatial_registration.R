866297a6ba2574b4e4b55e0ee844ac29f0bcfbe1
lcolladotor <lcolladotor@gmail.com>
2024-06-09 14:27:07 -0400

    Make a note about why we are renaming the object

--- 13_spatialLIBD_spatial_registration.R
+++ 13_spatialLIBD_spatial_registration.R
@@ -1,9 +1,9 @@
-## ----"download_modeling_results"--------------------------------------
+## ----"download_modeling_results"----------------------------------------------------
 ## get reference layer enrichment statistics
 layer_modeling_results <- spatialLIBD::fetch_data(type = "modeling_results")
 
 
-## ----"layer_modeling_results_workaround"------------------------------
+## ----"layer_modeling_results_workaround"--------------------------------------------
 tmp_modeling_results <- tempfile("modeling_results.RData")
 download.file(
     "https://www.dropbox.com/s/se6rrgb9yhm5gfh/Human_DLPFC_Visium_modeling_results.Rdata?dl=1",
@@ -11,5 +11,8 @@ download.file(
     mode = "wb"
 )
 load(tmp_modeling_results, verbose = TRUE)
+
+## Let's rename the object into the name used in the
+## spatial registration vignette (from spatialLIBD)
 layer_modeling_results <- modeling_results
 
