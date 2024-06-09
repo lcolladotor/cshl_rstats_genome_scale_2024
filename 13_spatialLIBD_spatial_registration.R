aaa16045d1a0561029022ef72bfe9c902d540dba
lcolladotor <lcolladotor@gmail.com>
2024-06-09 13:13:10 -0400

    Add a workaround to spatialLIBD::fetch_data("modeling_results") which might also fail due to the `curl` issue.

--- 13_spatialLIBD_spatial_registration.R
+++ 13_spatialLIBD_spatial_registration.R
@@ -1,1 +1,15 @@
+## ----"download_modeling_results"--------------------------------------
+## get reference layer enrichment statistics
+layer_modeling_results <- spatialLIBD::fetch_data(type = "modeling_results")
+
+
+## ----"layer_modeling_results_workaround"------------------------------
+tmp_modeling_results <- tempfile("modeling_results.RData")
+download.file(
+    "https://www.dropbox.com/s/se6rrgb9yhm5gfh/Human_DLPFC_Visium_modeling_results.Rdata?dl=1",
+    tmp_modeling_results,
+    mode = "wb"
+)
+load(tmp_modeling_results, verbose = TRUE)
+layer_modeling_results <- modeling_results
 
